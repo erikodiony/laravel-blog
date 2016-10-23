8884b5685757c7ed2749a95cdd4c954b53ccaf3e
erikodiony <erikodiony@live.com>
2016-10-23 16:19:56 +0700

    Add Stat

--- protected/app/Http/Controllers/ctrl_login.php
+++ protected/app/Http/Controllers/ctrl_login.php
@@ -16,7 +16,7 @@ class ctrl_login extends Controller
 {
     private function check_log()
     {        
-        $country = json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip={request()->ip()}"));
+        $country = json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".request()->ip()));
         if ($country->geoplugin_countryName == "")
         {
             $country_name = "-";
